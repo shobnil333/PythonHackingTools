a03ab3f4b155fc88720ed6fe0e7b141158ca7e97
MrRanGo <74337372+shobnil333@users.noreply.github.com>
2022-01-03 10:46:33 +0600

    Update CustomPasswordList.py

--- CustomPasswordList.py
+++ CustomPasswordList.py
@@ -3,7 +3,7 @@ from random import *
 
 characters = string.ascii_letters + string.digits + string.punctuation
 input_num = int(input('Enter number of password you want to generate: '))
-output = open(input('Enter a file name for your password file with extention(.txt): '), 'w')
+output = open(input('Enter a name for your password file with extention(.txt): '), 'w')
 
 for num in range(input_num):
     password = "".join(choice(characters) for i in range(randint(16, 20)))
